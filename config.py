eeeb1b9d9c865c35670d3f63000559e45724adb9
Akshatha M K <akshathamk@gmail.com>
2023-08-16 17:35:08 +0530

    add configs and read configs and env variables

---
+++ config.py
@@ -0,0 +1,20 @@
+import configparser
+import os
+
+def get_env():
+    """
+    read value of env from env variables and return the same
+    :return: env
+    """
+    return os.environ['ENV']
+
+def get_config(env):
+    """
+    read configs from properties.ini file and return the section correspondent to env value
+    :param env: section name
+    :return: configs
+    """
+    print(env)
+    configure = configparser.ConfigParser()
+    configure.read('properties.ini')
+    return configure[env]
